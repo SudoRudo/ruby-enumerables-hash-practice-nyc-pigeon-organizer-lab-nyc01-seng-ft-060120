f7ddb67c6134d79c94de03c4b70391c1a716b197
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-05-17 04:57:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,9 +16,11 @@ def nyc_pigeon_organizer(data)
   
   while i < birdNames.length do
     data.each do |val0, val1, val2|
+      data[val0].each do |val1, val2|
         if data[val0][val1].include?(birdNames[i])
           tempValues << val1.to_s
         end
+      end
       birdHash[birdNames[i]][val0] = tempValues
       tempValues = []
     end
